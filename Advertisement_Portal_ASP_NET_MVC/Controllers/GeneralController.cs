3d67ec660db396bd8218fc44cc31a6cfd62cda27
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-13 20:52:29 +0100

    Added file structure to general page views.

---
+++ Advertisement_Portal_ASP_NET_MVC/Controllers/GeneralController.cs
@@ -0,0 +1,40 @@
+﻿using Advertisement_Portal_ASP_NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Controllers
+{
+    public class GeneralController : Controller
+    {
+        private readonly ILogger<GeneralController> _logger;
+        public GeneralController(ILogger<GeneralController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult CompanyData()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Download()
+        {
+            return View();
+        }
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
