5c52c75f7e3fea57d9739dfdfbf80068e7d1f48e
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-13 20:15:09 +0100

    Add project files.

---
+++ Advertisement_Portal_ASP_NET_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Advertisement_Portal_ASP_NET_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
