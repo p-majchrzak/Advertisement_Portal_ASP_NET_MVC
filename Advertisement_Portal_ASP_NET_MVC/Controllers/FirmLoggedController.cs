2a8cbc196b2e1d7767f912ebd0ccc1fb70576e7f
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-15 14:28:55 +0100

    The structure of the Login file and styles for the elements of this file have been added.

---
+++ Advertisement_Portal_ASP_NET_MVC/Controllers/FirmLoggedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Controllers
+{
+    public class FirmLoggedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Advertisement_Portal_ASP_NET_MVC/Program.cs
+++ Advertisement_Portal_ASP_NET_MVC/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Load}/{action=Index}/{id?}");
+    pattern: "{controller=Firm}/{action=Login}/{id?}");
 
 app.Run();
