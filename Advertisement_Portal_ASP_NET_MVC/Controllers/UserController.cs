b46898924e8622c49d0edf16a1d10e9a3b0ba596
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-15 21:54:49 +0100

    Added file summary for user accounts.

---
+++ Advertisement_Portal_ASP_NET_MVC/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
