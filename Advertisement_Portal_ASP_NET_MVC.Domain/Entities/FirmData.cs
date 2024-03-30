8f1c36941fe415c1ff480654e75a87474bec0e6a
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-30 14:46:01 +0100

    FirmData class added.

---
+++ Advertisement_Portal_ASP_NET_MVC.Domain/Entities/FirmData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Domain.Entities
+{
+    public class FirmData
+    {
+        public required int ID { get; set; }
+        public string Name { get; set; } = default!;
+        public string Location { get; set; } = default!;
+        public int NIP { get; set; }
+        public int REGON { get; set; }
+        public string PhoneNubmer { get; set; } = default!;
+    }
+}
