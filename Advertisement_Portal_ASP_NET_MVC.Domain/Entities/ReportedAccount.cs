decbd6ac4c0570fc4185ac0ce69d5f8612f5f619
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-30 15:39:32 +0100

    ReportedAccount class added.

---
+++ Advertisement_Portal_ASP_NET_MVC.Domain/Entities/ReportedAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Domain.Entities
+{
+    public class ReportedAccount
+    {
+        public required int ID { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public UserData User { get; set; } = default!;
+    }
+}
