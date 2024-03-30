108208026f4243a66e739619ce8080117e0626d9
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-30 15:41:05 +0100

    ReportedAd class added.

---
+++ Advertisement_Portal_ASP_NET_MVC.Domain/Entities/ReportedAd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Domain.Entities
+{
+    public class ReportedAd
+    {
+        public required int ID { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public Announcement Ad { get; set; } = default!;
+    }
+}
