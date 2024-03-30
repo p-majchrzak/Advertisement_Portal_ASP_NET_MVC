fc6a864f329817470746d9371f15e80ad30303b4
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-30 15:11:45 +0100

    Message class added.

---
+++ Advertisement_Portal_ASP_NET_MVC.Domain/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Domain.Entities
+{
+    public class Message
+    {
+        public required int ID { get; set; }
+        public string Email { get; set; } = default!;
+        public string Content { get; set; } = default!; 
+    }
+}
