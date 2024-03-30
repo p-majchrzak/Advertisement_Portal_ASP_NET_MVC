c52c9962dbdbd9dc3223405c5ed76368a58f3c22
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-30 14:41:32 +0100

    Added database context.

---
+++ Advertisement_Portal_ASP_NET_MVC.Infrastructure/Persistance/Database.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement_Portal_ASP_NET_MVC.Infrastructure.Persistance
+{
+    public class Database : DbContext
+    {
+    }
+}
