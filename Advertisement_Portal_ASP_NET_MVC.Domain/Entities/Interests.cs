8e9c100ec6cf1e1432a1792375f8c622ed768e0c
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-30 15:35:27 +0100

    Interests class added.

--- Advertisement_Portal_ASP_NET_MVC.Domain/Entities/Interests.cs
+++ Advertisement_Portal_ASP_NET_MVC.Domain/Entities/Interests.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Advertisement_Portal_ASP_NET_MVC.Domain.Entities
 {
-    public class Languages
+    public class Interests
     {
         public required int ID { get; set; }
-        public string Name_Level { get; set; } = default!;
+        public string Content { get; set; } = default!;
         public UserData User { get; set; } = default!;
     }
 }
